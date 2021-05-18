9da420c886133ceb1cbe097bd30cc91adad3040c
José Luiz Borbolato <luizborbolato@gmail.com>
2021-05-18 00:22:05 -0300

    Commit do dia 18/05/2021

--- App.py
+++ App.py
@@ -1,1 +1,41 @@
-﻿print('Arquivo principal do projeto')
+﻿'''
+================================================================================
+Iterando sobre os dicionário com mais dicionários dentro =D
+================================================================================
+'''
+
+cliente = {
+    'C0001': {
+        'Nome': 'José Luiz',
+        'Idade': 29,
+        'endereco': {
+            'cep': '87301-320',
+            'rua': 'Rua das acacias',
+            'numero': 472,
+            'cidade': 'Campo Mourão'
+        }
+
+    },
+    'C0002': {
+        'Nome': 'João Vitor',
+        'Idade': 25
+
+    },
+    'C0003': {
+        'Nome': 'Pedro Vandame',
+        'Idade': 35,
+        'endereco': {
+            'cep': '87265-000',
+            'rua': 'Rua andromeda',
+            'numero': 51,
+            'cidade': 'Quinta do Sol'
+        }
+
+    }
+}
+
+for codigo, dados in cliente.items():
+    print(codigo)
+
+    for chave, dados_pessoas in dados.items():
+        print('\t', '->', dados_pessoas)
