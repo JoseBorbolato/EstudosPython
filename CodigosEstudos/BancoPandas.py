a3ac247c6e2bab675d392dbded15242ae4d82d41
José Luiz Borbolato <i17431@integrado.int>
2021-05-25 23:32:17 -0300

    25/05/2021

--- CodigosEstudos/BancoPandas.py
+++ CodigosEstudos/BancoPandas.py
@@ -3,6 +3,6 @@ from ConexoesBanco import *
 
 # PESQUISANDO TABELA GFILIAL E CRIANDO UM DATASET COM O RESULTADO DA PESQUISA
 GFILIAL = pd.read_sql_query('select * from GFILIAL', conexaoSQLServer())
-
+print(GFILIAL.head())
 # CRIANTO UMA TABELA A PARTIR DO RESULTADO DO DATASET RETORNADO DO OUTRO BANCO DE DADOS
-GFILIAL.to_sql('GFILIAL_CORPORE', con=conexaoSQLite())
+#GFILIAL.to_sql('GFILIAL', con=conexaoSQLite())
--- CodigosEstudos/EstudoPandas/read_xlsx.py
+++ CodigosEstudos/EstudoPandas/read_xlsx.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import numpy as np
 import os
+import sqlalchemy
+from sqlalchemy.engine import create_engine
 
-'''
-controle de diretório!
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
-'''
+# meu diretório padrão
+DIR_DEFAULT = os.path.abspath(__file__)
 
-abril = pd.read_excel(
-    'C:/Projetos/PythonProject/CodigosEstudos/EstudoPandas/arq/Abril.xlsx')
+# Nome do meu banco de dados
+BANCO = 'SQLiteCorporeRM.db'
+# string de conecção ao banco
+str_banco = f'sqlite:///{os.path.join(os.path.dirname(os.path.dirname(DIR_DEFAULT)),BANCO)}'
+# criando coneção
+engine = create_engine(str_banco, echo=False)
+# conectando ao banco de dados -> Criando banco
+engine = engine.connect()
 
-print(abril.head())
+
+# DEFININDO DIRETÓRIO DO ARQUIVO EXCEL
+DIR_DEFAULT = os.path.abspath(__file__)
+DIR_ARQ = os.path.join(os.path.dirname(DIR_DEFAULT), 'arq', 'Abril.xlsx')
+
+# LEITURA DA PLANILHA
+abril = pd.read_excel(DIR_ARQ)
+
+# TRANSFORMAÇÃO DO DATAFRAME EM TABELA NO BANCO DE DADOS
+abril.to_sql('REPASSE', engine)
