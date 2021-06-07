d4479073ebe3a7ac73bfaa44be45f601dc4e5afa
José Luiz Borbolato <luizborbolato@gmail.com>
2021-06-07 07:32:50 -0300

    Commit do dia 07/06

--- CodigosEstudos/EstudoPandas/read_xlsx.py
+++ CodigosEstudos/EstudoPandas/read_xlsx.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import os
-import sqlalchemy
 from sqlalchemy.engine import create_engine
 
 # meu diretório padrão
@@ -15,7 +14,6 @@ str_banco = f'sqlite:///{os.path.join(os.path.dirname(os.path.dirname(DIR_DEFAUL
 engine = create_engine(str_banco, echo=False)
 # conectando ao banco de dados -> Criando banco
 engine = engine.connect()
-
 
 # DEFININDO DIRETÓRIO DO ARQUIVO EXCEL
 DIR_DEFAULT = os.path.abspath(__file__)
